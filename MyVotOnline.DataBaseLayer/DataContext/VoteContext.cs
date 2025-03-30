22b4806fed7b55fc8fc91f4714c009664aed6c61
AjaySaini9582 <ajsaini9582@gmail.com>
2025-03-30 21:10:57 +0530

    context channges

--- MyVotOnline.DataBaseLayer/DataContext/VoteContext.cs
+++ MyVotOnline.DataBaseLayer/DataContext/VoteContext.cs
@@ -19,9 +19,9 @@ public partial class VoteContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=100.105.212.89;Port=5432;Database=VoteDb;Username=postgres;Password=Admin@9582");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseNpgsql("Host=100.105.212.89;Port=5432;Database=VoteDb;Username=postgres;Password=Admin@9582");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
